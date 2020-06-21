d10e0885b7ea3dc69c789ebd35fdeff58423f0d4
Tito Klautau <titoklautau@gmail.com>
2020-06-21 17:54:42 -0300

    implemented animal search loop and acess to csv

--- data.py
+++ data.py
@@ -7,11 +7,38 @@ class Data_module:
     def __init__(self):
         pass
 
+    def get_number_of_questions(self) -> int:
+        return None
+
+    def get_question_text_by_order(self, order_index: int) -> str:
+        return None
+
+    def remove_animals_by_question_order_value(self, question_order: int, value: int):
+        pass
+
+    def check_only_one_valid_animal_by_question_order(self, question_order: int) -> (bool, str):
+        return None, None
+
 class Csv_Data_module(Data_module):
     '''Csv using pandas data module'''
     mdl_type:str = 'Csv'
 
     def __init__(self):
         super().__init__()
-        self._animals_df = pd.read_csv('./dataframes/csv/animals.csv')
-        self._questions_df = pd.read_csv('./dataframes/csv/questions.csv')
+        self._animals_df = pd.read_csv('./dataframes/csv/animals.csv', index_col=0)
+        self._questions_df = pd.read_csv('./dataframes/csv/questions.csv', index_col=0)
+
+    def get_number_of_questions(self) -> int:
+        return len(self._questions_df)
+
+    def get_question_text_by_order(self, order_index: int) -> str:
+        return self._questions_df.loc[order_index, 'text']
+
+    def remove_animals_by_question_order_value(self, question_order:int, value: int):
+        self._animals_df = self._animals_df[~(self._animals_df[self._questions_df.loc[question_order].id] == value)]
+
+    def check_only_one_valid_animal_by_question_order(self, question_order: int) -> (bool, str):
+        if int(self._animals_df[self._questions_df.loc[question_order].id].sum()) == 1:
+            return True , self._animals_df.loc[self._animals_df[self._questions_df.loc[question_order].id] == 1, 'name'].item();
+        else:
+            return False, None;
--- logic.py
+++ logic.py
@@ -10,8 +10,43 @@ class Logic_module:
         self._data_module = data_module
 
     def start_execution(self):
+
+        #move the next 2 lines to ui module later
         self._ui_module.present_text('Think of an animal, then press Enter to continue:')
         self._ui_module.get_input()
 
+        flag, name = self._discover_animal()
+
+        if(flag):
+            #move nexts lines to ui module later
+            self._ui_module.present_text('Is the animal a ' + name + '?')
+
+        else:
+            self._ui_module.present_text('Sorry the animal you are thinking is not on my database, what was it?')
+
     def get_sup_modules_types(self):
         return(self._ui_module.mdl_type, self._data_module.mdl_type)
+
+    def _discover_animal(self) -> (bool, str):
+
+        for question_order_index in range(self._data_module.get_number_of_questions()):
+
+            #move this to ui module, as its ui dependent
+            self._ui_module.present_text('Does your animal ' + self._data_module.get_question_text_by_order(question_order_index) + '? (y/n)')
+            answer = self._ui_module.get_input()
+            while answer not in ('y','n','q'):
+                self._ui_module.present_text('Not a valid answer, insert a valid one')
+                answer = self._ui_module.get_input()
+            #end of block to pass to ui module
+
+            if answer == 'q':
+                break
+            else:
+                self._data_module.remove_animals_by_question_order_value(question_order_index, 0 if answer == 'y' else 1)
+
+            flag, name = self._data_module.check_only_one_valid_animal_by_question_order(question_order_index)
+
+            if(flag):
+                return True, name;
+
+        return False, None;
