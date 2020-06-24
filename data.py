3058fd80b58d610bd13dd3f1f1b2d3402a5c4a90
Tito Klautau <titoklautau@gmail.com>
2020-06-24 15:24:32 -0300

    finished basic logic

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 class Data_module:
     '''Base data manipulation module'''
@@ -7,10 +8,19 @@ class Data_module:
     def __init__(self):
         pass
 
+    def reset_animals_to_disk_version(self):
+        pass
+
+    def is_animal_present(self, name:str) -> bool:
+        return None
+
     def get_number_of_questions(self) -> int:
         return None
 
     def get_question_text_by_order(self, order_index: int) -> str:
+        return None
+
+    def get_question_id_by_order(self, order_index: int) -> str:
         return None
 
     def remove_animals_by_question_order_value(self, question_order: int, value: int):
@@ -18,6 +28,21 @@ class Data_module:
 
     def check_only_one_valid_animal_by_question_order(self, question_order: int) -> (bool, str):
         return None, None
+
+    def update_animal_with_dict(self, animal_data: dict):
+        pass
+
+    def add_animal_with_dict(self, animal_data: dict):
+        pass
+
+    def add_question_with_text(self, text: str) -> str:
+        return None
+
+    def save_animals_to_disk(self):
+        pass
+
+    def save_questions_to_disk(self):
+        pass
 
 class Csv_Data_module(Data_module):
     '''Csv using pandas data module'''
@@ -27,6 +52,14 @@ class Csv_Data_module(Data_module):
         super().__init__()
         self._animals_df = pd.read_csv('./dataframes/csv/animals.csv', index_col=0)
         self._questions_df = pd.read_csv('./dataframes/csv/questions.csv', index_col=0)
+        print(self._animals_df)
+        print(self._questions_df)
+
+    def reset_animals_to_disk_version(self):
+        self._animals_df = pd.read_csv('./dataframes/csv/animals.csv', index_col=0)
+
+    def is_animal_present(self, name: str) -> bool:
+        return not (self._animals_df.loc[self._animals_df.name == name].empty)
 
     def get_number_of_questions(self) -> int:
         return len(self._questions_df)
@@ -34,11 +67,38 @@ class Csv_Data_module(Data_module):
     def get_question_text_by_order(self, order_index: int) -> str:
         return self._questions_df.loc[order_index, 'text']
 
+    def get_question_id_by_order(self, order_index: int) -> str:
+        return self._questions_df.loc[order_index, 'id']
+
     def remove_animals_by_question_order_value(self, question_order:int, value: int):
         self._animals_df = self._animals_df[~(self._animals_df[self._questions_df.loc[question_order].id] == value)]
 
-    def check_only_one_valid_animal_by_question_order(self, question_order: int) -> (bool, str):
-        if int(self._animals_df[self._questions_df.loc[question_order].id].sum()) == 1:
-            return True , self._animals_df.loc[self._animals_df[self._questions_df.loc[question_order].id] == 1, 'name'].item();
+    def check_only_one_valid_animal_by_question_order(self, question_order: int) -> (int, str):
+        number_of_valids = len(self._animals_df[self._questions_df.loc[question_order].id].dropna())
+        if number_of_valids == 1:
+            return number_of_valids, self._animals_df.loc[self._animals_df[self._questions_df.loc[question_order].id].notnull(), 'name'].item();
         else:
-            return False, None;
+            return number_of_valids, None;
+
+    def update_animal_with_dict(self, animal_data: dict):
+        aux_series = pd.Series(animal_data)
+        self._animals_df.loc[self._animals_df.name == animal_data['name'], aux_series.index] = aux_series.values
+
+    def add_animal_with_dict(self, animal_data: dict):
+        self._animals_df = self._animals_df.append(animal_data, ignore_index=True)
+
+    def add_question_with_text(self, text:str) -> str:
+        if(not self._questions_df.loc[self._questions_df.text == text].empty):
+            return self._questions_df.loc[self._questions_df.text == text, 'id'].item()
+        else:
+            #the id management can cause problems if a question is removed, need to think of something better
+            aux_id = 'q'+str(len(self._questions_df.id))
+            self._questions_df = self._questions_df.append({'id':aux_id, 'text':text, 'order':len(self._questions_df.id)}, ignore_index=True)
+            self._animals_df[aux_id] = np.nan
+            return aux_id
+
+    def save_animals_to_disk(self):
+        self._animals_df.to_csv('./dataframes/csv/animals.csv')
+
+    def save_questions_to_disk(self):
+        self._questions_df.to_csv('./dataframes/csv/questions.csv')
--- logic.py
+++ logic.py
@@ -12,27 +12,71 @@ class Logic_module:
     def start_execution(self):
 
         #move the next 2 lines to ui module later
-        self._ui_module.present_text('Think of an animal, then press Enter to continue:')
-        self._ui_module.get_input()
+        self._ui_module.present_text('Think of an animal, then judge the facts about your animal.:')
 
-        flag, name = self._discover_animal()
+        name, answers_dict, number_of_valids = self._discover_animal()
 
-        if(flag):
+        if(name):
             #move nexts lines to ui module later
             self._ui_module.present_text('Is the animal a ' + name + '?')
+            answer = self._ui_module.get_input()
+            while answer not in ('y','n','q'):
+                self._ui_module.present_text('Not a valid answer, insert a valid one')
+                answer = self._ui_module.get_input()
+            if answer == 'q':
+                return
+            elif answer == 'y':
+                self._animal_found(name, answers_dict)
+            elif answer == 'n':
+                self._animal_not_found(answers_dict)
 
+        elif(number_of_valids):
+            self._ambiguous_animal_found(answers_dict, number_of_valids)
+        elif(answers_dict):
+            self._animal_not_found(answers_dict)
+
+    def _ambiguous_animal_found(self, answers_dict: dict, number_of_valids: int):
+        self._ui_module.present_text('There are ' + str(number_of_valids) + ' animals in my database that matches yours description, what is the name of the one you are thinking?')
+        name_aux = self._ui_module.get_input().lower()
+        if(name_aux == 'q'):
+            return
+        self._ui_module.present_text('Complete this blank space with an fact about your animal: Your animal ________ . ')
+        text_aux = self._ui_module.get_input()
+        if(text_aux == 'q'):
+            return
+        id_aux = self._data_module.add_question_with_text(text_aux)
+        answers_dict[id_aux] = 1
+        self._animal_found(name_aux, answers_dict)
+        self._data_module.save_questions_to_disk()
+        self._data_module.save_animals_to_disk()
+
+
+    def _animal_found(self, name: str, answers_dict: dict):
+            answers_dict['name'] = name;
+            self._data_module.update_animal_with_dict(answers_dict)
+            self._data_module.save_animals_to_disk()
+
+    def _animal_not_found(self, answers_dict:dict):
+        self._ui_module.present_text('Sorry the animal you are thinking is not on my database or I dont have enough information on it to give a definitive answer, what is the animal?')
+        name_aux = self._ui_module.get_input().lower()
+        if(self._data_module.is_animal_present(name_aux)):
+            self._animal_found(name_aux, answers_dict)
         else:
-            self._ui_module.present_text('Sorry the animal you are thinking is not on my database, what was it?')
+            answers_dict['name'] = name_aux
+            self._data_module.add_animal_with_dict(answers_dict)
+            self._data_module.save_animals_to_disk()
+
 
     def get_sup_modules_types(self):
         return(self._ui_module.mdl_type, self._data_module.mdl_type)
 
-    def _discover_animal(self) -> (bool, str):
+    def _discover_animal(self) -> (str,dict,int):
 
+        answers_dict = {}
         for question_order_index in range(self._data_module.get_number_of_questions()):
 
             #move this to ui module, as its ui dependent
-            self._ui_module.present_text('Does your animal ' + self._data_module.get_question_text_by_order(question_order_index) + '? (y/n)')
+            self._ui_module.present_text('Your animal ' + self._data_module.get_question_text_by_order(question_order_index) + '. (y/n)')
             answer = self._ui_module.get_input()
             while answer not in ('y','n','q'):
                 self._ui_module.present_text('Not a valid answer, insert a valid one')
@@ -40,13 +84,17 @@ class Logic_module:
             #end of block to pass to ui module
 
             if answer == 'q':
-                break
+                self._data_module.reset_animals_to_disk_version()
+                return None, None, None;
             else:
                 self._data_module.remove_animals_by_question_order_value(question_order_index, 0 if answer == 'y' else 1)
+                answers_dict[self._data_module.get_question_id_by_order(question_order_index)] = 1 if answer == 'y' else 0
 
-            flag, name = self._data_module.check_only_one_valid_animal_by_question_order(question_order_index)
+            number_of_valids, name = self._data_module.check_only_one_valid_animal_by_question_order(question_order_index)
 
-            if(flag):
-                return True, name;
+            if(number_of_valids == 1):
+                self._data_module.reset_animals_to_disk_version()
+                return name, answers_dict, number_of_valids;
 
-        return False, None;
+        self._data_module.reset_animals_to_disk_version()
+        return None, answers_dict, number_of_valids;
