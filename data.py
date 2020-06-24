b87cdd2d9057823c86cc3ee1f2122f6c77e20822
Tito Klautau <titoklautau@gmail.com>
2020-06-24 20:46:29 -0300

    added weights for the question ordering

--- data.py
+++ data.py
@@ -17,16 +17,16 @@ class Data_module:
     def get_number_of_questions(self) -> int:
         return None
 
-    def get_question_text_by_order(self, order_index: int) -> str:
+    def get_question_text_by_index(self, index: int) -> str:
         return None
 
-    def get_question_id_by_order(self, order_index: int) -> str:
+    def get_question_id_by_index(self, index: int) -> str:
         return None
 
-    def remove_animals_by_question_order_value(self, question_order: int, value: int):
+    def remove_animals_by_question_index_value(self, question_index: int, value: int):
         pass
 
-    def check_only_one_valid_animal_by_question_order(self, question_order: int) -> (bool, str):
+    def check_only_one_valid_animal_by_question_index(self, question_index: int) -> (bool, str):
         return None, None
 
     def update_animal_with_dict(self, animal_data: dict):
@@ -44,6 +44,12 @@ class Data_module:
     def save_questions_to_disk(self):
         pass
 
+    def calculate_questions_weights(self):
+        pass
+
+    def randomize_questions(self):
+        pass
+
 class Csv_Data_module(Data_module):
     '''Csv using pandas data module'''
     mdl_type:str = 'Csv'
@@ -52,8 +58,6 @@ class Csv_Data_module(Data_module):
         super().__init__()
         self._animals_df = pd.read_csv('./dataframes/csv/animals.csv', index_col=0)
         self._questions_df = pd.read_csv('./dataframes/csv/questions.csv', index_col=0)
-        print(self._animals_df)
-        print(self._questions_df)
 
     def reset_animals_to_disk_version(self):
         self._animals_df = pd.read_csv('./dataframes/csv/animals.csv', index_col=0)
@@ -64,19 +68,19 @@ class Csv_Data_module(Data_module):
     def get_number_of_questions(self) -> int:
         return len(self._questions_df)
 
-    def get_question_text_by_order(self, order_index: int) -> str:
-        return self._questions_df.loc[order_index, 'text']
+    def get_question_text_by_index(self, index: int) -> str:
+        return self._questions_df.loc[index, 'text']
 
-    def get_question_id_by_order(self, order_index: int) -> str:
-        return self._questions_df.loc[order_index, 'id']
+    def get_question_id_by_index(self, index: int) -> str:
+        return self._questions_df.loc[index, 'id']
 
-    def remove_animals_by_question_order_value(self, question_order:int, value: int):
-        self._animals_df = self._animals_df[~(self._animals_df[self._questions_df.loc[question_order].id] == value)]
+    def remove_animals_by_question_index_value(self, question_index:int, value: int):
+        self._animals_df = self._animals_df[~(self._animals_df[self._questions_df.loc[question_index].id] == value)]
 
-    def check_only_one_valid_animal_by_question_order(self, question_order: int) -> (int, str):
-        number_of_valids = len(self._animals_df[self._questions_df.loc[question_order].id].dropna())
+    def check_only_one_valid_animal_by_question_index(self, question_index: int) -> (int, str):
+        number_of_valids = len(self._animals_df[self._questions_df.loc[question_index].id].dropna())
         if number_of_valids == 1:
-            return number_of_valids, self._animals_df.loc[self._animals_df[self._questions_df.loc[question_order].id].notnull(), 'name'].item();
+            return number_of_valids, self._animals_df.loc[self._animals_df[self._questions_df.loc[question_index].id].notnull(), 'name'].item();
         else:
             return number_of_valids, None;
 
@@ -92,8 +96,8 @@ class Csv_Data_module(Data_module):
             return self._questions_df.loc[self._questions_df.text == text, 'id'].item()
         else:
             #the id management can cause problems if a question is removed, need to think of something better
-            aux_id = 'q'+str(len(self._questions_df.id))
-            self._questions_df = self._questions_df.append({'id':aux_id, 'text':text, 'order':len(self._questions_df.id)}, ignore_index=True)
+            aux_id = 'q'+str(self._questions_df.order.max()+1)
+            self._questions_df = self._questions_df.append({'id':aux_id, 'text':text, 'order':self._questions_df.order.max()+1, 'weight':0}, ignore_index=True)
             self._animals_df[aux_id] = np.nan
             return aux_id
 
@@ -102,3 +106,13 @@ class Csv_Data_module(Data_module):
 
     def save_questions_to_disk(self):
         self._questions_df.to_csv('./dataframes/csv/questions.csv')
+
+    def calculate_questions_weights(self):
+        for row in self._questions_df.index:
+            aux = self._animals_df.loc[:,self._questions_df.loc[row].id]
+            self._questions_df.loc[row,'weight'] = (1 - abs(len(aux.loc[aux == 1]) - len(aux.loc[aux == 0]))/len(aux.dropna())) * (len(aux.dropna())/len(aux))
+            self._questions_df = self._questions_df.sort_values('weight', ascending=False).reset_index(drop=True)
+            self.save_questions_to_disk()
+
+    def randomize_questions(self):
+        self._questions_df = self._questions_df.sample(frac=1).reset_index(drop=True)
